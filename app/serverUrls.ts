faaed0531258c1e7aa116887614cb418fe0cbf33
Mael <laem@kont.me>
2025-02-05 15:30:30 +0100

    Intégration de l'URL de déploiement DOKPLOY créée suite à ma demande

--- app/serverUrls.ts
+++ app/serverUrls.ts
@@ -21,7 +21,8 @@ export const getFetchUrlBase = () => {
 	// Coolify has a similar way to Vercel to inject the domain, but Dokploy has not
 	if (givenDomain) return 'https://' + givenDomain
 
-	const branchUrl = process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+	const branchUrl =
+		process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL || process.env.DOKPLOY_DEPLOY_URL
 	const isMaster = branchUrl?.includes('-git-master-')
 	const domain = isMaster ? process.env.NEXT_PUBLIC_BASE_DOMAIN : branchUrl
 
