36bacf83c54a664e3abf0ac9f4c8302e4ed9571a
Mael <laem@kont.me>
2025-02-10 18:10:40 +0100

    Essai d'une autre technique pour passer la var d'env

--- instrumentation.ts
+++ instrumentation.ts
@@ -8,8 +8,6 @@ export async function register() {
 	if (process.env.NEXT_RUNTIME === 'edge') {
 		await import('./sentry.edge.config')
 	}
-
-	process.env.NEXT_PUBLIC_DOKPLOY_DEPLOY_URL = process.env.DOKPLOY_DEPLOY_URL
 }
 
 export const onRequestError = Sentry.captureRequestError
