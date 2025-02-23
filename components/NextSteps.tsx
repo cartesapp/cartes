a62323e9e9a2126e16f1e5d6a257e3956071f689
Mael <laem@kont.me>
2025-02-23 11:38:57 +0100

    On essaie de faire marcher les comments github pour y récupérer l'effort

--- components/NextSteps.tsx
+++ components/NextSteps.tsx
@@ -8,15 +8,16 @@ export default function NextSteps({ issues: givenIssues }) {
 	const issues = Array.isArray(givenIssues) ? givenIssues : [+givenIssues]
 
 	const selected = list.filter((issue) => issues.includes(issue.number))
+	console.log('indigo2', selected.length)
 
 	useEffect(() => {
 		const doFetch = async () => {
 			const commentEntries = await Promise.all(
 				selected.map(async ({ number, title, html_url, body }) => {
 					try {
-						const request = await fetch(
-							`https://api.github.com/repos/cartesapp/cartes/issues/${number}/comments`
-						)
+						return
+						const url = `https://api.github.com/repos/cartesapp/cartes/issues/${number}/comments`
+						const request = await fetch(url)
 
 						const json = await request.json()
 						return [number, json]
