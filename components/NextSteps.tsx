14062372a047d70153252d8be924981bb8390057
Mael <laem@kont.me>
2025-02-23 11:06:28 +0100

    Récupération des commentaires des issues

--- components/NextSteps.tsx
+++ components/NextSteps.tsx
@@ -1,3 +1,41 @@
-export default function NextSteps({ issues }) {
-	return <div>{issues}</div>
+'use client'
+
+import list from '@/public/github-issues.json'
+import { useEffect, useState } from 'react'
+
+export default function NextSteps({ issues: givenIssues }) {
+	const [comments, setComments] = useState({})
+	const issues = Array.isArray(givenIssues) ? givenIssues : [+givenIssues]
+
+	const selected = list.filter((issue) => issues.includes(issue.number))
+
+	useEffect(() => {
+		const doFetch = async () => {
+			const commentEntries = await Promise.all(
+				selected.map(async ({ number, title, html_url, body }) => {
+					try {
+						const request = await fetch(
+							`https://api.github.com/repos/cartesapp/cartes/issues/${number}/comments`
+						)
+
+						const json = await request.json()
+						return [number, json]
+					} catch (e) {
+						return
+					}
+				})
+			)
+
+			setComments(Object.fromEntries(commentEntries))
+		}
+
+		doFetch()
+	}, [selected, setComments])
+
+	if (selected.length !== issues.length) return 'Au moins un ticket non trouvé'
+
+	console.log('indigo', comments)
+
+	return
+	return <div>{issue['title']}</div>
 }
