003a41e813235bf1bc32b3b2c306d3b97a7d668e
Mael <laem@kont.me>
2025-02-25 10:16:04 +0100

    Metadonnées pour roadmap

--- app/roadmap/page.tsx
+++ app/roadmap/page.tsx
@@ -3,6 +3,12 @@ import Page, { title, description } from './Page'
 export const metadata: Metadata = {
 	title,
 	description,
+	openGraph: {
+		images: ['/blog-images/construction'],
+		url: '/roadmap',
+		modifiedTime: '2025-02-25T00:00:00.000ZT00:00:00.000Z',
+		type: 'article',
+	},
 }
 
 export default function () {
