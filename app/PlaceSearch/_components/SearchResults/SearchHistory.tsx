a4985cb0399b6444c1b6b14aea1407afc74c8ff8
Mael <laem@kont.me>
2025-02-28 15:40:49 +0100

    Passage à next-yak propre dans SearchHistory

--- app/PlaceSearch/_components/SearchResults/SearchHistory.tsx
+++ app/PlaceSearch/_components/SearchResults/SearchHistory.tsx
@@ -1,6 +1,6 @@
+import { css, styled } from 'next-yak'
 import Image from 'next/image'
 import SearchResultsContainer from './SearchResultsContainer'
-import { css } from 'next-yak'
 
 export default ({
 	sideSheet,
@@ -10,18 +10,7 @@ export default ({
 }) => {
 	return (
 		<SearchResultsContainer $sideSheet={sideSheet}>
-			<div
-				css={css`
-					display: flex;
-					justify-content: center;
-					align-items: center;
-					img {
-						width: 0.9rem;
-						height: auto;
-						vertical-align: sub;
-					}
-				`}
-			>
+			<Header>
 				<small>Historique</small>
 				<button
 					onClick={() => setSearchHistory([])}
@@ -29,29 +18,40 @@ export default ({
 				>
 					<Image src="/trash.svg" width="10" height="10" alt="Icône poubelle" />
 				</button>
-			</div>
-			<ul
-				css={css`
-					li:not(:last-of-type) {
-						border-bottom: 1px solid var(--lightestColor);
-					}
-					li {
-						padding: 0.4rem 0;
-						list-style-type: none;
-						font-size: 90%;
-						line-height: 130%;
-					}
-					li:hover {
-						background: var(--lightestColor);
-					}
-				`}
-			>
+			</Header>
+			<List>
 				{searchHistory.map((entry) => (
 					<li key={entry}>
 						<button onClick={() => onDestinationChange(entry)}>{entry}</button>
 					</li>
 				))}
-			</ul>
+			</List>
 		</SearchResultsContainer>
 	)
 }
+
+const List = styled.ol`
+	li:not(:last-of-type) {
+		border-bottom: 1px solid var(--lightestColor);
+	}
+	li {
+		padding: 0.4rem 0;
+		list-style-type: none;
+		font-size: 90%;
+		line-height: 130%;
+	}
+	li:hover {
+		background: var(--lightestColor);
+	}
+`
+
+const Header = styled.header`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	img {
+		width: 0.9rem;
+		height: auto;
+		vertical-align: sub;
+	}
+`
