b4c4f2f0db9f23834b77109721a518cce58bb248
Mael <laem@kont.me>
2025-03-20 10:10:59 +0100

    Texte de description de carte plus resseré et plus séparé

--- components/MapContent.tsx
+++ components/MapContent.tsx
@@ -9,10 +9,11 @@ export default function MapContent({ content }) {
 }
 
 const P = styled.p`
+	margin-top: 1.6rem;
 	font-size: 85%;
 	color: var(--darkerColor);
 	opacity: 0.8;
-	line-height: 1.2rem;
+	line-height: 1rem;
 `
 
 const Wrapper = styled.section``
