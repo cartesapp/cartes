ce7350d1ca893d75cfc5de45e854ea43bf1b3dcd
Mael <laem@kont.me>
2025-04-01 11:34:58 +0200

    Revert "WIP logo à gauche sur bureau"
    This reverts commit 01e456a49499d37c0063ec110908c4fef0f2f120.

--- app/PlaceSearch/_components/LogoCarteApp.tsx
+++ app/PlaceSearch/_components/LogoCarteApp.tsx
@@ -1,23 +1,11 @@
 import Logo from '@/public/logo.svg'
-import { styled } from 'next-yak'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default ({ link }) => {
 	return (
-		<LogoLink title="À propos de Cartes" href={link}>
+		<Link title="À propos de Cartes" href={link}>
 			<Image src={Logo} alt="Logo de Cartes.app" width="100" height="100" />
-		</LogoLink>
+		</Link>
 	)
 }
-
-const LogoLink = styled(Link)`
-	margin: 0;
-	padding: 0;
-	margin-right: 0.4rem;
-	> img {
-		width: 2rem;
-		height: auto;
-		vertical-align: middle;
-	}
-`
