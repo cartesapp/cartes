e4dfef28df0e0a7b17d77035300170472e6a2e1c
Mael <laem@kont.me>
2025-04-03 19:04:46 +0200

    Segmentation du code

--- app/effects/useDrawSearchResults.ts
+++ app/effects/useDrawSearchResults.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
-import { combinedOsmRequest } from '../osmRequest'
 import useDrawFeatures from './useDrawFeatures'
+import combinedOsmFeaturesRequest from '@/components/osm/combinedOsmFeaturesRequest'
 
 export default function useDrawSearchResults(map, state, setOsmFeature) {
 	// Photon search results are not full OSM objectfs, lacking tags, so lacking
@@ -17,7 +17,7 @@ export default function useDrawSearchResults(map, state, setOsmFeature) {
 		if (!results) return
 
 		const doFetch = async () => {
-			const newFeatures = await combinedOsmRequest(results)
+			const newFeatures = await combinedOsmFeaturesRequest(results)
 			setFeatures(newFeatures)
 		}
 		doFetch()
---
+++ components/osm/combinedOsmFeaturesRequest.ts
@@ -0,0 +1,45 @@
+import { overpassRequestSuffix } from '@/app/osmRequest'
+
+export default async function combinedOsmFeaturesRequest(queries) {
+	const requestBody = queries
+		.map((result) => {
+			const { osmId, featureType, latitude, longitude } = result
+
+			return `${featureType}(id:${osmId}); out body; `
+		})
+		.join('')
+
+	const requestString = `[out:json];${requestBody}`
+	const url = overpassRequestSuffix + encodeURIComponent(requestString)
+
+	const request = await fetch(url, {
+		next: { revalidate: 5 * 60 },
+	})
+
+	const json = await request.json()
+
+	const { elements } = json
+
+	const results = queries
+		.map((query) => {
+			const found = elements.find(
+				(element) =>
+					query.osmId === element.id && query.featureType === element.type
+			)
+
+			if (!found) return false
+			const geoElement = {
+				...found,
+				lat: query.latitude,
+				lon: query.longitude,
+			}
+			return geoElement
+		})
+		.filter(Boolean)
+	console.log('requestString', requestString, results)
+
+	//TODO we don't handle housenumbers like in osmRequest, not sure we need this
+	//in this combinedOsmRequest function that is used to enrich photon search
+	//results with OSM tags
+	return results
+}
