6d3e9274f97403478405e74dc910744e07db2c27
Mael <laem@kont.me>
2025-04-21 20:58:55 +0200

    :bug: Changement de style avec dessin activé

--- app/effects/useTerraDraw.ts
+++ app/effects/useTerraDraw.ts
@@ -1,37 +1,44 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import '@watergis/maplibre-gl-terradraw/dist/maplibre-gl-terradraw.css'
 import { MaplibreMeasureControl } from '@watergis/maplibre-gl-terradraw'
 
+const createControl = (storeControl) => {
+	const control = new MaplibreMeasureControl({
+		modes: [
+			'render',
+			'point',
+			'linestring',
+			'polygon',
+			'rectangle',
+			'circle',
+			'freehand',
+			'angled-rectangle',
+			'sensor',
+			'sector',
+			'select',
+			'delete-selection',
+			'delete',
+			'download',
+		],
+		open: true,
+		distanceUnit: 'kilometers',
+		areaUnit: 'metric',
+		computeElevation: true,
+	})
+
+	control.fontGlyphs = ['RobotoRegular-NotoSansRegular']
+
+	storeControl(control)
+	return control
+}
+
 export default function useTerraDraw(map, dessin) {
+	const [storedControl, storeControl] = useState(null)
 	useEffect(() => {
 		if (!map) return
 		if (!dessin) return
 
-		const control = new MaplibreMeasureControl({
-			modes: [
-				'render',
-				'point',
-				'linestring',
-				'polygon',
-				'rectangle',
-				'circle',
-				'freehand',
-				'angled-rectangle',
-				'sensor',
-				'sector',
-				'select',
-				'delete-selection',
-				'delete',
-				'download',
-			],
-			open: true,
-			distanceUnit: 'kilometers',
-			areaUnit: 'metric',
-			computeElevation: true,
-		})
-
-		control.fontGlyphs = ['RobotoRegular-NotoSansRegular']
-
+		const control = storedControl || createControl(storeControl)
 		map.addControl(control, 'top-left')
 
 		return () => {
@@ -41,5 +48,5 @@ export default function useTerraDraw(map, dessin) {
 				console.log('Trying to remove terrain 3D control failed', e)
 			}
 		}
-	}, [map, dessin])
+	}, [map, dessin, storedControl])
 }
