a3d5e6e790bbe12ef034277fdd0606719e75ba9c
Mael <laem@kont.me>
2025-05-14 18:27:42 +0200

    Pour lutter contre les bots, on réécrit les URL problématiques
    Je pense que les bots se sont faits une queue d'URL à la combinatoire
    explosive et balancent ensuite les requêtes. Je dois donc les empêcher
    d'y accéder.

--- middleware.ts
+++ middleware.ts
@@ -7,8 +7,35 @@ export function middleware(request: NextRequest) {
 	console.log('url ', url.href, url.searchParams.toString())
 	console.log('IsBot ', UA.isBot, ' UA ', UA.ua)
 
-	const response = NextResponse.next()
-	return response
+	const { searchParams } = new URL(request.url)
+
+	// Get the 'cat' parameter
+	const catParam = searchParams.get('cat')
+
+	// Check if the 'cat' parameter exists
+	if (catParam) {
+		// Decode the 'cat' parameter
+		const decodedCatParam = decodeURIComponent(catParam)
+
+		// Split the decoded parameter by pipe to get individual categories
+		const categories = decodedCatParam.split('|')
+
+		// Check if there is more than one category
+		if (categories.length > 1) {
+			// Get the first category
+			const firstCategory = categories[0]
+
+			// Create a new URL with only the first category
+			const newUrl = new URL(request.url)
+			newUrl.searchParams.set('cat', firstCategory)
+
+			// Redirect to the new URL
+			return NextResponse.redirect(newUrl)
+		}
+	}
+
+	// If there's only one or no 'cat' parameter, continue with the request
+	return NextResponse.next()
 }
 
 // See "Matching Paths" below to learn more
