4c4454bbc17a3b5967a300ce68ced5050594d971
Mael <laem@kont.me>
2025-05-19 15:19:13 +0200

    Moved the GTFS color functions to a new file

--- app/itinerary/transit/motisRequest.ts
+++ app/itinerary/transit/motisRequest.ts
@@ -7,6 +7,7 @@ import { lightenColor } from '@/components/utils/colors'
 import { distance, point } from '@turf/turf'
 import transportIcon from './transportIcon'
 import { datePlusHours, defaultRouteColor, nowStamp, stamp } from './utils'
+import { handleColor, trainColors } from './colors'
 
 // For onTrip, see https://github.com/motis-project/motis/issues/471#issuecomment-2247099832
 const buildRequestBody = (start, destination, date, searchParams) => {
@@ -259,29 +260,6 @@ export const computeMotisTrip = async (
 	}
 }
 
-export const trainColors = {
-	TGV: { color: '#b8175e' },
-	OUIGO: { color: '#e60075', border: '#0096ca' },
-	'Train TER': { color: '#004da4' },
-	TER: { color: '#004da4' },
-	'TGV INOUI': { color: '#9b2743' },
-	'INTERCITES de nuit': { color: '#28166f' },
-	INTERCITES: { color: 'Teal' },
-	'Car TER': { color: '#004da4', pointillés: true },
-	Car: { color: '#004da4', pointillés: true },
-	Lyria: { color: '#eb0a28' },
-	ICE: { color: '#f01414' },
-	TramTrain: { color: '#004da4' },
-	Navette: { color: '#004da4', pointillés: true },
-}
-export function handleColor(rawColor, defaultColor) {
-	if (!rawColor) return defaultColor
-	if (rawColor.startsWith('#')) return rawColor
-	if (rawColor.match(/^([0-9A-Fa-f])+$/) && rawColor.length === 6)
-		return '#' + rawColor
-	console.log('Unrecognized route color', rawColor)
-	return defaultColor
-}
 export { stamp }
 
 const notTransitType = ['Walk', 'Cycle', 'Car']
