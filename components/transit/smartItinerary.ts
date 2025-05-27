97b5db4c4c241f9177b4bd77b16adaaad396d658
Mael <laem@kont.me>
2025-05-27 10:16:24 +0200

    Déplacement des commentaires de modes.ts à smartItinerary

--- components/transit/smartItinerary.ts
+++ components/transit/smartItinerary.ts
@@ -1,14 +1,35 @@
 import { computeMotisTrip } from '@/app/itinerary/transit/motisRequest'
 
-export function unsatisfyingItineraries(json, itineraryDistance) {
-	if (!json.itineraries.length) return true
-
-	//TODO we could use here more sophisticated algorithms to solve the frequent
-	//problem of buses that make detours just to take a train. It's easy if the
-	//detour is replacable by the bike max time set in useFetchItinerary, but it
-	//would fail by just one minute ! We'd have to analyse the shape of the
-	//transit.
-}
+// If no mode is set by the user, we're trying to provide a multimodal view
+// that covers a large range of possibilities, giving the user perspectives
+// that we cannot guess precisely.
+//
+// Motis v1 (outdated)
+// This is the state of the art of our comprehension of how to use Motis to
+// produce useful intermodal results in France, letting the user find the
+// closest train station for more long range requests
+// See https://github.com/cartesapp/cartes/issues/416
+//
+// Here we set a threshold in km (50) for either not asking a trip starting with a bike
+// segment because we expect the user will use local transit, or ask it with a
+// max bike duration request depending on the distance :
+// 1h of bike ~= 20km for trips lower than 200 km
+// 2h and 40 km for trips more than 200 km
+//
+// With thiese settings, we should cover most of the hexagone.
+//const bikeTrainSearchDistance = //0 * 60
+//	hours(distance < 10 ? 0 : distance < 200 ? 1 : 2)
+//
+// **Motis v2** cannot set a specific max_duration per pre-transit mode. It's
+// shared. So we cannot ask Motis to give us walk trips with max 10 minutes walk
+// + bike trips with 30 minutes, for our "by default" large cover mode
+//
+// WALK is 2 km/h
+// BIKE is 20 km/h, x10
+// CAR is 50 to 100 km/h, x3 to x5, so final x 30 or x 50
+//
+// This leads to the necessity to make multiple parallel requests and then
+// analyse and summarize the results
 
 const computeDistanceAndSpeed = (json, itineraryDistance) => {
 	const realDistance =
@@ -24,6 +45,11 @@ const computeDistanceAndSpeed = (json, itineraryDistance) => {
 	}, json.itineraries[0])
 
 	const speed = fastest && distance / (fastest.duration / (60 * 60))
+	//TODO we could use here more sophisticated algorithms to solve the frequent
+	//problem of buses that make detours just to take a train. It's easy if the
+	//detour is replacable by the bike max time set in useFetchItinerary, but it
+	//would fail by just one minute ! We'd have to analyse the shape of the
+	//transit.
 
 	console.log('cyan unsatisfyingItineraries', json, speed)
 	return [distance, speed]
