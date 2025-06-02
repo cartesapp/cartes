dc229653a4d432e5cda86dd656dc05f0ba0dd65e
Mael <laem@kont.me>
2025-06-02 12:20:38 +0200

    Nouvelle route trouver adresse

---
+++ app/trouver-adresse-annonce/page.tsx
@@ -0,0 +1,11 @@
+export default function Trouver() {
+	return (
+		<div>
+			<iframe
+				src="https://www.ouestfrance-immo.com/immobilier/vente/maison/rennes-35-35238/18866115.htm"
+				width="800px"
+				height="1000px"
+			/>
+		</div>
+	)
+}
