a49070ad24a70c0a16f2bf35423ed2534c2903c5
Mael <laem@kont.me>
2025-06-02 12:39:21 +0200

    Suite
    La stratégie via iframe est vouée à l'échec, ils peuvent être bloqués.
    Globalement, le faire à partir d'une URL d'annonce me semble trop
    compliqué, même si évidemment faisable plus tard.
    
    Demander plutôt à l'utilisateur de saisir les données : surface ; DPE
    lettre ou numéro ; point estimé sur la carte.
    
    Afficher les résultats en vue satellite. Proposer des liens Streetview
    si pas de Panoramax.

--- app/trouver-adresse-annonce/page.tsx
+++ app/trouver-adresse-annonce/page.tsx
@@ -2,7 +2,7 @@ export default function Trouver() {
 	return (
 		<div>
 			<iframe
-				src="https://www.ouestfrance-immo.com/immobilier/vente/maison/rennes-35-35238/18866115.htm"
+				src="https://www.leboncoin.fr/ad/ventes_immobilieres/2997757709"
 				width="800px"
 				height="1000px"
 			/>
