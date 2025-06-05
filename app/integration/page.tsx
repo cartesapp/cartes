7d2fcb294a51ecb7c5c85ff2229368577149de76
Mael <laem@kont.me>
2025-06-05 14:29:24 +0200

    Remise en arrière d'un commit erroné

--- app/integration/page.tsx
+++ app/integration/page.tsx
@@ -1,9 +1,6 @@
 import { Metadata } from 'next'
 import { description } from '../layout'
-import { title } from './Page'
-import { PresentationWrapper } from './UI'
-import Image from 'next/image'
-import Logo from '@/public/logo.svg'
+import Page, { title } from './Page'
 
 export const metadata: Metadata = {
 	title,
@@ -11,24 +8,7 @@ export const metadata: Metadata = {
 }
 
 export default function () {
-	return (
-		<PresentationWrapper>
-			<section>
-				<header>
-					<h1>
-						<Image src={Logo} alt="Logo de cartes.app" /> Cartes
-					</h1>
-				</header>
-			</section>
-			<h2>Intégrez une carte à votre site en 30 secondes</h2>
-
-			<p>
-				Vous êtes un club sportif, une association, un commerce, une
-				administration avec des locaux ouverts au public ? Il est important de
-				montrer une carte à vos utilisateurs.{' '}
-			</p>
-		</PresentationWrapper>
-	)
+	return <Page />
 }
 
 // https://jhildenbiddle.github.io/css-device-frames/#/
